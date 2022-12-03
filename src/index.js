e0022ebbaf9545ec98910abe4e9527d8173f1c03
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-12-03 18:11:11 +0100

    Added database connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const debug = require("debug")("news-api:root");
 const chalk = require("chalk");
+const connectDB = require("./database");
 
 const initializeServer = require("./server/initializeServer");
 
 const port = process.env.PORT ?? 4000;
+const connectionString = process.env.MONGODB_STRING;
 
 (async () => {
   try {
+    await connectDB(connectionString);
     await initializeServer(port);
   } catch {
     debug(chalk.red("Exiting with errors"));
