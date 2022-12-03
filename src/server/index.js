33c7cbba229f56538b6fea1a81189e55e072a7f1
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-12-03 15:57:13 +0100

    Added cors, morgan and helmet middlewares

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
 
 module.exports = app;
