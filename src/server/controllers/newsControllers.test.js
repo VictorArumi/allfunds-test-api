6a3901032d8a31d27ac722f76e71730b8f44fcf4
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-12-05 17:50:55 +0100

    Created and tested set new to archived controller middleware

--- src/server/controllers/newsControllers.test.js
+++ src/server/controllers/newsControllers.test.js
@@ -1,6 +1,6 @@
 const { mockNews } = require("../../database/mocks/mockNews");
 const New = require("../../database/models/New");
-const { getNews } = require("./newsControllers");
+const { getNews, setNewToArchived } = require("./newsControllers");
 
 const res = {
   status: jest.fn().mockReturnThis(),
@@ -12,6 +12,11 @@ jest.mock("../../database/models/New", () => ({
   populate: jest.fn().mockResolvedValue(mockNews),
   sort: jest.fn().mockReturnThis(),
   find: jest.fn().mockReturnThis(),
+  findByIdAndUpdate: jest.fn().mockResolvedValue({
+    ...mockNews[0],
+    archived: true,
+    archiveDate: Date("2022-01-31T23:00:00.000Z"),
+  }),
 }));
 
 const next = jest.fn();
@@ -39,6 +44,44 @@ describe("given a getNews function", () => {
       });
 
       await getNews(req, res, next);
+
+      expect(next).toHaveBeenCalledWith(expectedError);
+    });
+  });
+});
+
+describe("given a setNewToArchived function", () => {
+  describe("When its invoked with a request with new id param", () => {
+    test("Then it should call the response's methods status with '200' and json with a new", async () => {
+      const idToEdit = mockNews[0].id;
+      const expectedStatusCode = 200;
+      const expectedJson = {
+        updatedNew: {
+          ...mockNews[0],
+          archived: true,
+          archiveDate: Date("2022-01-31T23:00:00.000Z"),
+        },
+      };
+
+      const req = { params: { id: idToEdit } };
+
+      await setNewToArchived(req, res, null);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+      expect(res.json).toHaveBeenCalledWith(expectedJson);
+    });
+  });
+
+  describe("When its invoked an invalid id", () => {
+    test("Then it should call the next with an error", async () => {
+      const idToEdit = false;
+      const req = { params: { id: idToEdit } };
+
+      const expectedError = new Error();
+
+      New.findByIdAndUpdate.mockResolvedValue(null);
+
+      await setNewToArchived(req, res, next);
 
       expect(next).toHaveBeenCalledWith(expectedError);
     });
