15a487b99015b84f855c0818489eb1c91dda7f9a
VictorArumi <99591372+VictorArumi@users.noreply.github.com>
2022-12-05 17:51:53 +0100

    Added setNewToArchive middleware to router, tested /news/edit/:id endpoint

--- src/server/routers/newsRouter.test.js
+++ src/server/routers/newsRouter.test.js
@@ -23,8 +23,8 @@ afterAll(async () => {
 });
 
 describe("Given a GET /news enpoint", () => {
-  describe("When it receives a request", () => {
-    test("Then oit should return the database list of news with archived false value, sorted by storage date", async () => {
+  describe("When it receives a valid request", () => {
+    test("Then it should return status 200 and the database list of news with archived false value, sorted by storage date", async () => {
       await New.create(mockNews[0]);
       await New.create(mockNews[1]);
       await New.create(mockNews[2]);
@@ -42,6 +42,44 @@ describe("Given a GET /news enpoint", () => {
       expect(news[0].storageDate).toBe(sortedNonArchivedNews[0].storageDate);
       expect(news[1].description).toBe(sortedNonArchivedNews[1].description);
       expect(news[1].storageDate).toBe(sortedNonArchivedNews[1].storageDate);
+    });
+  });
+});
+
+describe("Given a PUT /news/edit/:id enpoint", () => {
+  describe("When it receives a request with an existing new id", () => {
+    test("Then it should return status 200 and the same new with archive property value true, and an archiveDate", async () => {
+      const { id: idToArchive } = await New.create(mockNews[0]);
+
+      const newSetToArchived = {
+        ...mockNews[0],
+        archived: true,
+        archiveDate: Date.now(),
+      };
+
+      const {
+        body: { updatedNew },
+      } = await request(app).put(`/news/edit/${idToArchive}`).expect(200);
+
+      expect(updatedNew.archived).toBe(true);
+      expect(updatedNew.archiveDate).toBeTruthy();
+      expect(updatedNew.title).toBe(newSetToArchived.title);
+      expect(updatedNew.storageDate).toBe(newSetToArchived.storageDate);
+    });
+  });
+
+  describe("When it receives a request with an non existent new id", () => {
+    test("Then it should return status 404 and the error message:'Error: new id does not exist in DB, impossible to update' ", async () => {
+      await New.create(mockNews[0]);
+      const idToArchive = "this is a non existent id";
+      const expectedMsg =
+        "Error: new id does not exist in DB, impossible to update";
+
+      const {
+        body: { msg },
+      } = await request(app).put(`/news/edit/${idToArchive}`).expect(404);
+
+      expect(msg).toBe(expectedMsg);
     });
   });
 });
